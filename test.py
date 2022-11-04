a3f7bf0e4f526a3ae97234ba58d45b0d2f8fdf29
zach jones <zach@jonesaz.com.com>
2022-11-04 06:44:17 -0700

    fud

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 #https://www.youtube.com/watch?v=-ykeT6kk4bk
+#24:54
 app = FastAPI()
 #uvicorn test:app --reload
-@app.get("https://randomfox.ca/floof")
-async def root():
-    return {"message": "Hello World"}
+inventory = {
+    1:{
+        "name" : "MILK",
+        "price " : 3.99,
+        "brand " : " regular"
+    }
+}
+
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None,description = "the id of the idem ")):
+
+    async def root():
+        return {"message": "Hello World"}
+        return inventory[item_id]
  
