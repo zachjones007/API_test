b1edb86c54f2b71bfaebca3c018f15b77e721a07
zach jones <zach@jonesaz.com.com>
2022-11-04 06:30:22 -0700

    fu

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-
+#https://www.youtube.com/watch?v=-ykeT6kk4bk
 app = FastAPI()
-
+#uvicorn test:app --reload
 @app.get("https://randomfox.ca/floof")
 async def root():
     return {"message": "Hello World"}
+ 
