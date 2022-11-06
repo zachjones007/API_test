d9ec3d97e17f582eefb105a2994629695bbf6934
zach <zach@joneasaz.com>
2022-11-05 23:35:39 -0700

    127.0.0.1 wont work

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI, Path
 #https://www.youtube.com/watch?v=-ykeT6kk4bk
 #24:54
 app = FastAPI()
+#how to load up a :8000
+#http://127.0.0.1:8000 
 #uvicorn test:app --reload
+
 inventory = {
     1:{
         "name" : "MILK",
@@ -17,4 +20,10 @@ def get_item(item_id: int = Path(None,description = "the id of the idem ")):
     async def root():
         return {"message": "Hello World"}
         return inventory[item_id]
- 
+
+@app.get("/get-by-name")
+def get_item(name: str):
+    for item_id in inventory:
+        if inventory[item_id]["name"] == name:
+            return {"Data" : "not Found"}
+
