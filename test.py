155b0bb11d5def4dcddaacfb70da450f3c5d1f92
zach <zach@joneasaz.com>
2022-11-06 06:45:27 -0700

    working

--- test.py
+++ test.py
@@ -5,12 +5,14 @@ import uvicorn
 app = FastAPI()
 #how to load up a :8000
 #http://localhost:8080
+#http://localhost:8080/docs
+#http://localhost:8080/get-item/1/test
 #uvicorn main:app --port 8086  --reload
 1
 
-#@app.get("/")
-#async def root():
-#   return {"message": "Hello_Wosrl1d"}
+@app.get("/")
+async def root():
+   return {"message": "Hello_Wosrl1d"}
 
 
 
@@ -21,3 +23,13 @@ async def root():
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
 
+inventory = {
+    1: {
+        "name" : "Milk",
+        "price" : 3.99,
+        "brand" : "Regular"
+    }
+}
+@app.get("/get-item/{item_id}")
+def get_item(item_id:int, name:str ):
+    return inventory[item_id].update
